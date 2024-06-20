f8b0512924b91b97688e9e04d64dd0b8dff94763
Flávio Henrique <fla-henrique@hotmail.com>
2024-06-20 17:33:25 -0300

    atualizado gitignore

--- config/config.php
+++ config/config.php
@@ -13,10 +13,10 @@ define('DIRJS',DIRPAGE.'lib/js/');
 define('DIRCONT',DIRPAGE.'controllers/');
 
 #Acesso ao BD
-define('HOST',"localhost");
-define('BD',"qualidade");
-define('USER',"root");
-define('PASS',"");
+define('HOST',"sql209.infinityfree.com");
+define('BD',"epiz_33972719_sistema");
+define('USER',"epiz_33972719");
+define('PASS',"42B7derpCH");
 
 include(DIRREQ."ignore/dadosUser.php");
 #Informações do servidor de email
